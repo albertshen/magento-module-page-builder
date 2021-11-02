ed8e053a42112417da4c1bc2f379e9c769478626
Albert Shen <albertshen1206@gmail.com>
2021-11-02 17:35:18 +0800

    up

--- Controller/Adminhtml/File/Upload.php
+++ Controller/Adminhtml/File/Upload.php
@@ -3,7 +3,7 @@
  * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
-namespace Albert\Magento\PageBuilder\Controller\Adminhtml\File;
+namespace AlbertMage\Controller\Adminhtml\File;
 
 use Magento\Framework\App\Action\HttpPostActionInterface;
 use Magento\Framework\App\Filesystem\DirectoryList;
